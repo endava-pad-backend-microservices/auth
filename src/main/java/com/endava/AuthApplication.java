7260f951f4bc184db30ab9f3c20dbec355fe3381
ayancovich <ayancovich@endava.com>
2019-05-15 16:38:59 -0300

    Added swagger 2 to auth

--- src/main/java/com/endava/AuthApplication.java
+++ src/main/java/com/endava/AuthApplication.java
@@ -9,9 +9,15 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan({"com.endava.controller","com.endava.provider","com.endava.bean","com.endava.serviceimpl"})
+@EnableSwagger2
 public class AuthApplication {
 
 	public static void main(String[] args) {
@@ -25,6 +31,18 @@ public class AuthApplication {
 		public RestTemplate restTemplate(RestTemplateBuilder builder) {
 			return builder.build();
 		}
+	}
+	
+	@Configuration
+	@EnableSwagger2
+	public class SwaggerConfig {
+	    @Bean
+	    public Docket authAPI() {
+	        return new Docket(DocumentationType.SWAGGER_2)
+	                .select()                 
+	                .apis(RequestHandlerSelectors.basePackage("com.endava.controller"))
+	                .build();
+	    }
 	}
 
 }
