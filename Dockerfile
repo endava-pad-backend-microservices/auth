4a9ee3e348b04cd41a3a29e4b93771c1da32b837
ayancovich <ayancovich@endava.com>
2019-06-03 17:45:41 -0300

    Removed if sentence, returning stream instead.

--- Dockerfile
+++ Dockerfile
@@ -9,18 +9,23 @@ RUN mvn package
 FROM openjdk:12 as Target
 COPY --from=builder /build/target/auth-1.0.0.jar auth.jar
 
-ENV MONGO_URL=pad-b-auth-database \
- EUREKA_URL=pad-b-registry \
- SERVER_URL=pad-b-auth \
- MONGO_DB=admin \
- MONGO_USER=admin \
- MONGO_PASSWORD=123456 \
- DOCKERIZE_VERSION=v0.6.0\
- MONGO_PORT=27018
+ENV MONGO_URL=pad-b-auth-database \ 
+    EUREKA_URL=pad-b-registry \ 
+    SERVER_URL=pad-b-auth \ 
+    MONGO_DB=admin \ 
+    MONGO_USER=admin \ 
+    MONGO_PASSWORD=123456 \ 
+    DOCKERIZE_VERSION=v0.6.0 \ 
+    MONGO_PORT=27018 \ 
+    CONFIG_URL=pad-b-config \ 
+    CONFIG_PORT=8086
 
 CMD wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-alpine-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
-&& tar -C /usr/local/bin -xzvf dockerize-alpine-linux-amd64-$DOCKERIZE_VERSION.tar.gz \ 
-&& rm dockerize-alpine-linux-amd64-$DOCKERIZE_VERSION.tar.gz && dockerize -wait tcp://$EUREKA_URL:8761 -timeout 60m yarn start && dockerize -wait tcp://$MONGO_URL:$MONGO_PORT -timeout 60m yarn start
+    && tar -C /usr/local/bin -xzvf dockerize-alpine-linux-amd64-$DOCKERIZE_VERSION.tar.gz \ 
+    && rm dockerize-alpine-linux-amd64-$DOCKERIZE_VERSION.tar.gz \ 
+    && dockerize -wait tcp://$EUREKA_URL:8761 -timeout 60m yarn start \ 
+    && dockerize -wait tcp://$MONGO_URL:$MONGO_PORT -timeout 60m yarn start \
+    && dockerize -wait tcp://$CONFIG_URL:$CONFIG_PORT -timeout 60m yarn start
 
 ENTRYPOINT ["java","-jar","/auth.jar"]
 
--- src/main/java/com/endava/serviceimpl/TokenService.java
+++ src/main/java/com/endava/serviceimpl/TokenService.java
@@ -1,29 +1,27 @@
 package com.endava.serviceimpl;
 
-import org.apache.commons.lang.StringUtils;
-import org.json.simple.JSONObject;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+import java.util.Arrays;
 
-import com.endava.bean.SecurityConstants;
 import com.endava.provider.JwtTokenProvider;
 import com.endava.service.ITokenService;
-import com.netflix.discovery.EurekaClient;
+
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Service;
 
 import io.jsonwebtoken.JwtException;
 
 @Service
+@RefreshScope
 public class TokenService implements ITokenService {
 
 	@Autowired
 	private JwtTokenProvider jwtTokenProvider;
 
-	@Autowired
-	private EurekaClient eurekaClient;
-
-	@Autowired
-	private RestTemplate restTemplate;
+	@Value("#{'${auth.endpoints}'.split(',')}")
+	private String[] excludes_urls;
 
 	public boolean checkToken(JSONObject data) {
 		String token = data.get("token").toString();
@@ -31,19 +29,17 @@ public class TokenService implements ITokenService {
 
 		if (token != null) {
 			if (!jwtTokenProvider.isTokenPresentInDB(token)) {
-				return false;//throw new RuntimeException("Invalid JWT token");
+				return false;// throw new RuntimeException("Invalid JWT token");
 			}
 			try {
 				jwtTokenProvider.validateToken(token);
 			} catch (JwtException | IllegalArgumentException e) {
-				return false;//throw new RuntimeException("Invalid JWT token");
+				return false;// throw new RuntimeException("Invalid JWT token");
 			}
 		}
 
 		if (url != null) {
-			if (StringUtils.endsWithAny(url, SecurityConstants.UNSECURED_URLS)) {
-				return false;
-			}
+			return !Arrays.stream(excludes_urls).anyMatch(x -> url.equals(x.toString()));
 		}
 		return true;
 	}
