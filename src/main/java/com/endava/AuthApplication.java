ff140281963774045a9723a89bfab6cb5abea94b
ayancovich <ayancovich@endava.com>
2019-06-14 10:47:48 -0300

    Added tracing MS

--- src/main/java/com/endava/AuthApplication.java
+++ src/main/java/com/endava/AuthApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import brave.sampler.Sampler;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -43,6 +44,11 @@ public class AuthApplication {
 	                .apis(RequestHandlerSelectors.basePackage("com.endava.controller"))
 	                .build();
 	    }
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
